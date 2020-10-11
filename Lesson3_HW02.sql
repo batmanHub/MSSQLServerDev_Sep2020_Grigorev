1235ff313419e3cbf2affcdb5c4da664db3b2e98
batmanHub <batman12321@yandex.ru>
2020-10-11 14:05:43 +0300

    Update Lesson3_HW02.sql

--- Lesson3_HW02.sql
+++ Lesson3_HW02.sql
@@ -2,17 +2,185 @@
 1. Выберите сотрудников (Application.People), которые являются продажниками (IsSalesPerson), и не сделали ни одной продажи 04 июля 2015 года. Вывести ИД сотрудника и его полное имя. Продажи смотреть в таблице Sales.Invoices.
 */
 
+--1.1 вложенный
+select 
+People.FullName
+,IsSalesperson
+from 
+Application.People as People
+where IsSalesperson = 1 AND People.PersonID  <> ALL   
+	(Select	Distinct
+	Invoices.SalespersonPersonID 
+	From Sales.Invoices
+	INNER JOIN Application.People ON People.PersonID = Invoices.SalespersonPersonID
+	Where Invoices.InvoiceDate  between '2015-04-01' and '2015-04-30'
+	);
+
+--delete from Sales.CustomerTransactions where CustomerTransactions.InvoiceID in (select Invoices.InvoiceID from Sales.Invoices where Invoices.InvoiceDate  between '2015-04-01' and '2015-04-30' and SalespersonPersonID in (7,2))
+--delete from Sales.InvoiceLines where InvoiceID in (select Invoices.InvoiceID from Sales.Invoices where Invoices.InvoiceDate  between '2015-04-01' and '2015-04-30' and SalespersonPersonID in (7,2))
+--delete from Warehouse.StockItemTransactions where InvoiceID in (select Invoices.InvoiceID from Sales.Invoices where Invoices.InvoiceDate  between '2015-04-01' and '2015-04-30' and SalespersonPersonID in (7,2))
+--delete from Sales.Invoices where Invoices.InvoiceDate  between '2015-04-01' and '2015-04-30' and SalespersonPersonID in (7,2)
+
+--1.2 через CTE
+;with sellersIds as (
+	Select	Distinct
+	Invoices.SalespersonPersonID
+	From Sales.Invoices
+	Where Invoices.InvoiceDate  between '2015-04-01' and '2015-04-30'
+)
+select 
+People.FullName
+from 
+Application.People as People
+where IsSalesperson = 1 AND People.PersonID  <> ALL (select * from sellersIds)
+
 /*
 2. Выберите товары с минимальной ценой (подзапросом). Сделайте два варианта подзапроса. Вывести: ИД товара, наименование товара, цена.
 */
+--2.1 подзапрос
+select 
+StockItemID
+,StockItemName
+,UnitPrice
+from
+[Warehouse].[StockItems] 
+where UnitPrice  <=  ALL (
+	select (UnitPrice) FROM
+	[Warehouse].[StockItems]
+	)
+
+--2.2 с CTE 
+; WITH minPrices as 
+(
+select MIN(UnitPrice) as minPrice
+FROM [Warehouse].[StockItems]
+)
+select 
+StockItemID
+,StockItemName
+,UnitPrice
+from [Warehouse].[StockItems] 
+where UnitPrice <= (select top (1) minPrice from  minPrices)
+
 
 /*
 3. Выберите информацию по клиентам, которые перевели компании пять максимальных платежей из Sales.CustomerTransactions. Представьте несколько способов (в том числе с CTE).
 */
+--3.1 Вложенный запрос, если клиенты повторяются, будет меньше 5 клиентов 
+ 
+select 
+CustomerID
+,CustomerName
+,PhoneNumber
+from Sales.Customers 
+where Customers.CustomerID in (
+	select top 5 
+	CustomerID
+	from
+	Sales.CustomerTransactions
+	where IsFinalized = 1
+	order by TransactionAmount desc)
+
+--3.2 CTE, +выберем 5 уникальных клиентов
+;With CustomerIDsMaxTrans as 
+(
+select top 5 
+	CustomerID as CustomerID
+	,MAX(TransactionAmount) as TransactionAmount
+	from
+	Sales.CustomerTransactions
+	where IsFinalized = 1
+	group by CustomerID
+	order by MAX(TransactionAmount) desc
+)
+select 
+CustomerID
+,CustomerName
+,PhoneNumber
+from Sales.Customers 
+where Customers.CustomerID in (select CustomerIDsMaxTrans.CustomerID from CustomerIDsMaxTrans)
+
 
 /*
 4. Выберите города (ид и название), в которые были доставлены товары, входящие в тройку самых дорогих товаров, а также имя сотрудника, который осуществлял упаковку заказов (PackedByPersonID).
 */
+--4.1 Вариант с временными таблицами, с агрегацией имен сотрудников в одну строку
+IF OBJECT_ID('tempdb.dbo.#ExpensiveStockItems', 'U') IS NOT NULL
+  DROP TABLE #ExpensiveStockItems; 
+IF OBJECT_ID('tempdb.dbo.#InvoicesIds', 'U') IS NOT NULL
+  DROP TABLE #InvoicesIds; 
+IF OBJECT_ID('tempdb.dbo.#InvoicesData', 'U') IS NOT NULL
+  DROP TABLE #InvoicesData; 
+
+-- выберем 3 дорогих товара
+select top 3
+	StockItemID
+	,StockItemName
+	,UnitPrice
+INTO #ExpensiveStockItems
+from
+[Warehouse].[StockItems] 
+order by UnitPrice desc;
+
+--выберем уникальные Id накладных с дорогими товарами 
+Select Distinct
+	InvoiceID
+INTO #InvoicesIds
+from Sales.InvoiceLines
+where InvoiceLines.StockItemID IN (Select StockItemID FROM #ExpensiveStockItems);
+
+--Выберем ID города и Имя упаковавшего накладную 
+Select 
+  Customers.DeliveryCityID
+  ,People.FullName as PackedPeopleName
+INTO #InvoicesData
+from Sales.Invoices 
+LEFT JOIN Sales.Customers ON Customers.CustomerID = Invoices.CustomerID
+LEFT JOIN Application.People ON People.PersonID = Invoices.PackedByPersonID
+Where InvoiceID IN (select #InvoicesIds.InvoiceID from #InvoicesIds)
+--GROUP BY DeliveryCityID, People.FullName
+--Order by DeliveryCityID, PackedPeopleName;
+select * from #InvoicesData where DeliveryCityID = 15
+--4.1итоговый запрос
+select 
+	 CityID
+	,CityName
+	,#InvoicesData.PackedPeopleName
+	--,String_AGG(#InvoicesData.PackedPeopleName,', ') as [List of persons packing the invoices]
+from Application.Cities 
+INNER JOIN #InvoicesData ON #InvoicesData.DeliveryCityID = Cities.CityID
+--Group by CityID,CityName
+
+--4.2 СTE + подзапросы, без агригации имен сотрудников
+
+;With SelectInvoicesIds as (
+Select 
+	Customers.DeliveryCityID
+	,People.FullName as PackedPeopleName
+	from Sales.Invoices 
+	LEFT JOIN Sales.Customers ON Customers.CustomerID = Invoices.CustomerID
+	LEFT JOIN Application.People ON People.PersonID = Invoices.PackedByPersonID
+	Where InvoiceID IN (
+		select  Distinct
+		InvoiceID
+		from Sales.InvoiceLines
+		where InvoiceLines.StockItemID IN (
+			Select top 3
+				StockItemID 
+			FROM [Warehouse].[StockItems] 
+			order by UnitPrice desc
+			)
+		)
+	GROUP BY Customers.DeliveryCityID, People.FullName
+	--Order by DeliveryCityID, PackedPeopleName
+)
+select 
+	SelectInvoicesIds.DeliveryCityID
+	,Cities.CityName
+	,SelectInvoicesIds.PackedPeopleName
+from SelectInvoicesIds
+inner join Application.Cities ON Cities.CityID = SelectInvoicesIds.DeliveryCityID
+Order by DeliveryCityID,PackedPeopleName
 
 /*
 Опционально:
