743b0e281c497cef550c51e66f6e26ee3db9c785
batmanHub <batman12321@yandex.ru>
2020-10-12 23:57:57 +0300

    Update Lesson3_HW02.sql

--- Lesson3_HW02.sql
+++ Lesson3_HW02.sql
@@ -4,8 +4,8 @@
 
 --1.1 вложенный
 select 
-People.FullName
-,IsSalesperson
+People.PersonID
+,People.FullName
 from 
 Application.People as People
 where IsSalesperson = 1 AND People.PersonID  <> ALL   
@@ -114,21 +114,23 @@ IF OBJECT_ID('tempdb.dbo.#InvoicesData', 'U') IS NOT NULL
 
 -- выберем 3 дорогих товара
 select top 3
-	StockItemID
+	StockItems.StockItemID
 	,StockItemName
 	,UnitPrice
 INTO #ExpensiveStockItems
 from
 [Warehouse].[StockItems] 
-order by UnitPrice desc;
+order by UnitPrice desc,StockItemID;
+select * from #ExpensiveStockItems
 
 --выберем уникальные Id накладных с дорогими товарами 
 Select Distinct
-	InvoiceID
+	InvoiceLines.InvoiceID
+	,StockItemID
 INTO #InvoicesIds
 from Sales.InvoiceLines
 where InvoiceLines.StockItemID IN (Select StockItemID FROM #ExpensiveStockItems);
-
+select * from #InvoicesIds order by StockItemID
 --Выберем ID города и Имя упаковавшего накладную 
 Select 
   Customers.DeliveryCityID
@@ -137,46 +139,47 @@ INTO #InvoicesData
 from Sales.Invoices 
 LEFT JOIN Sales.Customers ON Customers.CustomerID = Invoices.CustomerID
 LEFT JOIN Application.People ON People.PersonID = Invoices.PackedByPersonID
-Where InvoiceID IN (select #InvoicesIds.InvoiceID from #InvoicesIds)
---GROUP BY DeliveryCityID, People.FullName
+Where Invoices.InvoiceID IN (select #InvoicesIds.InvoiceID from #InvoicesIds)
+GROUP BY DeliveryCityID, People.FullName
 --Order by DeliveryCityID, PackedPeopleName;
-select * from #InvoicesData where DeliveryCityID = 15
+--select * from #InvoicesData where DeliveryCityID = 15
+
 --4.1итоговый запрос
 select 
 	 CityID
 	,CityName
-	,#InvoicesData.PackedPeopleName
-	--,String_AGG(#InvoicesData.PackedPeopleName,', ') as [List of persons packing the invoices]
+	,String_AGG(#InvoicesData.PackedPeopleName,', ') as [List of persons packing the invoices]
 from Application.Cities 
 INNER JOIN #InvoicesData ON #InvoicesData.DeliveryCityID = Cities.CityID
---Group by CityID,CityName
+Group by CityID,CityName
 
 --4.2 СTE + подзапросы, без агригации имен сотрудников
-
 ;With SelectInvoicesIds as (
 Select 
 	Customers.DeliveryCityID
 	,People.FullName as PackedPeopleName
-	from Sales.Invoices 
+	,Invoices.PackedByPersonID
+	from   Sales.Invoices
 	LEFT JOIN Sales.Customers ON Customers.CustomerID = Invoices.CustomerID
 	LEFT JOIN Application.People ON People.PersonID = Invoices.PackedByPersonID
-	Where InvoiceID IN (
+	Where Invoices.InvoiceID IN (
 		select  Distinct
-		InvoiceID
+		InvoiceLines.InvoiceID
 		from Sales.InvoiceLines
-		where InvoiceLines.StockItemID IN (
+		where InvoiceLines.StockItemID in(
 			Select top 3
-				StockItemID 
-			FROM [Warehouse].[StockItems] 
-			order by UnitPrice desc
+				Items.StockItemID 
+			FROM [Warehouse].[StockItems] as Items
+			order by Items.UnitPrice desc,Items.StockItemID
 			)
 		)
-	GROUP BY Customers.DeliveryCityID, People.FullName
+	GROUP BY Customers.DeliveryCityID, Invoices.PackedByPersonID,People.FullName
 	--Order by DeliveryCityID, PackedPeopleName
 )
 select 
 	SelectInvoicesIds.DeliveryCityID
 	,Cities.CityName
+	,SelectInvoicesIds.PackedByPersonID
 	,SelectInvoicesIds.PackedPeopleName
 from SelectInvoicesIds
 inner join Application.Cities ON Cities.CityID = SelectInvoicesIds.DeliveryCityID
@@ -185,7 +188,10 @@ Order by DeliveryCityID,PackedPeopleName
 /*
 Опционально:
 
-5. Объясните, что делает и оптимизируйте запрос:
+5. Объясните, что делает и оптимизируйте запрос:*/
+SET STATISTICS IO, TIME ON
+GO
+
 SELECT
 Invoices.InvoiceID,
 Invoices.InvoiceDate,
@@ -209,5 +215,43 @@ GROUP BY InvoiceId
 HAVING SUM(Quantity*UnitPrice) > 27000) AS SalesTotals
 ON Invoices.InvoiceID = SalesTotals.InvoiceID
 ORDER BY TotalSumm DESC
-Можно двигаться как в сторону улучшения читабельности запроса, так и в сторону упрощения плана\ускорения. Сравнить производительность запросов можно через SET STATISTICS IO, TIME ON. Если знакомы с планами запросов, то используйте их (тогда к решению также приложите планы). Напишите ваши рассуждения по поводу оптимизации. 
+--Можно двигаться как в сторону улучшения читабельности запроса, так и в сторону упрощения плана\ускорения. Сравнить производительность запросов можно через SET STATISTICS IO, TIME ON. Если знакомы с планами запросов, то используйте их (тогда к решению также приложите планы). Напишите ваши рассуждения по поводу оптимизации. 
+
+/*вариант 5.1
+Запрос выбирает ид накладной, дату накладной, Имя продавца,  сумму товаров по накладной и сумму товаров из связанного заказа (если комплектация заказа завершена), если сумма накладной больше 27000 
+Сделал с временной таблицей и CTE
 */
+IF OBJECT_ID('tempdb.dbo.#SalesTotals', 'U') IS NOT NULL
+  DROP TABLE #SalesTotals; 
+
+SELECT 
+InvoiceId, 
+SUM(Quantity*UnitPrice) AS TotalSumm
+into #SalesTotals
+FROM Sales.InvoiceLines
+GROUP BY InvoiceId
+HAVING SUM(Quantity*UnitPrice) > 27000
+
+;with
+OrdersTotals as (
+SELECT 
+OrderLines.OrderId,
+SUM(OrderLines.PickedQuantity*OrderLines.UnitPrice) as TotalSummForPickedItems
+FROM  Sales.Orders
+INNER JOIN Sales.OrderLines ON Orders.OrderID = OrderLines.OrderID
+WHERE Orders.PickingCompletedWhen IS NOT NULL
+GROUP BY OrderLines.OrderId
+)
+
+Select
+Invoices.InvoiceID,
+Invoices.InvoiceDate,
+SalesPerson.FullName as SalesPersonName,
+#SalesTotals.TotalSumm AS TotalSummByInvoice,
+OrdersTotals.TotalSummForPickedItems
+FROM Sales.Invoices
+INNER JOIN Application.People as SalesPerson ON SalesPerson.PersonID = Invoices.SalespersonPersonID
+INNER JOIN #SalesTotals ON #SalesTotals.InvoiceID = Invoices.InvoiceID
+LEFT JOIN OrdersTotals  ON #SalesTotals.InvoiceID = OrdersTotals.OrderID 
+ORDER BY TotalSumm DESC
+
