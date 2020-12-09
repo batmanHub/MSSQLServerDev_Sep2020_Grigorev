4eb2e3bc0dbc29582118474f68372dfb4dda7f5d
batmanHub <batman12321@yandex.ru>
2020-12-09 04:57:13 +0300

    Update Lesson12_HW08.sql

--- Lesson12_HW08.sql
+++ Lesson12_HW08.sql
@@ -1,42 +1,62 @@
 /*
-1) Ќаписать функцию возвращающую  лиента с наибольшей суммой покупки.
+1) Написать функцию возвращающую  лиента с наибольшей суммой покупки.
 */
 
-IF OBJECT_ID ( 'dbo.GetCustomerByMaxSum', 'P' ) IS NOT NULL   
-    DROP PROCEDURE dbo.GetCustomerByMaxSum;  
-GO 
-
-SET ANSI_NULLS ON
-GO
-SET QUOTED_IDENTIFIER ON
-GO
+IF  OBJECT_ID ( 'dbo.uFN_GetCustomerIDByMaxSum','FN' ) IS NOT NULL   
+    DROP FUNCTION dbo.uFN_GetCustomerIDByMaxSum;  
+GO  
+IF  OBJECT_ID ( 'dbo.uFN_GetCustomerIDByMaxSum_withMax','FN' ) IS NOT NULL   
+    DROP FUNCTION dbo.uFN_GetCustomerIDByMaxSum_withMax;  
+GO  
 -- =============================================
 -- Description: ”ровень изол¤ции : Read Committed
 -- =============================================
-CREATE PROCEDURE GetCustomerByMaxSum
-	@topCustomersCount int = 1
+CREATE FUNCTION uFN_GetCustomerIDByMaxSum ()
+RETURNS int
 AS
 BEGIN
-	SET NOCOUNT ON;
-	Select TOP (@topCustomersCount) 
-		cust.CustomerName
-		,SUM (UnitPrice * Quantity) as SaleSum
-	FROM Sales.Customers as cust
-	JOIN Sales.Orders on Orders.CustomerID = cust.CustomerID
-	JOIN Sales.OrderLines ON Orders.OrderID = OrderLines.OrderID
-    GROUP BY Orders.CustomerID,cust.CustomerName
-	ORDER BY SUM (UnitPrice * Quantity) DESC
-END
+	Declare @CustomerID int;
+
+	Set @CustomerID = ( select  TopCustomer.CustomerID FROM (
+		Select TOP (1) 
+			Orders.CustomerID
+			,SUM (UnitPrice * Quantity) as SaleSum
+		FROM Sales.Customers as cust
+		JOIN Sales.Orders on Orders.CustomerID = cust.CustomerID
+		JOIN Sales.OrderLines ON Orders.OrderID = OrderLines.OrderID
+		GROUP BY Orders.CustomerID
+		ORDER BY SUM (UnitPrice * Quantity) DESC
+		) as TopCustomer)
+	Return (@CustomerID)
+END;
+GO
+CREATE FUNCTION uFN_GetCustomerIDByMaxSum_withMax ()
+RETURNS int
+AS
+BEGIN
+	Declare @CustomerID int;
+
+	Set @CustomerID = ( select  
+	Max (TopCustomer.CustomerID)
+	FROM (
+		Select  
+			Orders.CustomerID as CustomerID 
+			,SUM (UnitPrice * Quantity) as SaleSum
+		FROM Sales.Customers as cust
+		JOIN Sales.Orders on Orders.CustomerID = cust.CustomerID
+		JOIN Sales.OrderLines ON Orders.OrderID = OrderLines.OrderID
+		GROUP BY Orders.CustomerID
+		) as TopCustomer
+	Having TopCustomer.SaleSum = Max
+	)
+	Return (@CustomerID)
+END;
 GO
 
+select dbo.uFN_GetCustomerIDByMaxSum () as TopCustomer
+select dbo.uFN_GetCustomerIDByMaxSum_withMax () as TopCustomer2
 
-Exec GetCustomerByMaxSum 
-Go
-
-Exec GetCustomerByMaxSum  @topCustomersCount=5
-GO
-
-/*2) Ќаписать хранимую процедуру с вход¤щим параметром —ustomerID, вывод¤щую сумму покупки по этому клиенту.
+/*2) Написать хранимую процедуру с вход¤щим параметром CustomerID, выводящую сумму покупки по этому клиенту.
 */
 
 IF OBJECT_ID ( 'dbo.GetCustomerSalesSumByID', 'P' ) IS NOT NULL   
@@ -49,7 +69,7 @@ GO
 SET QUOTED_IDENTIFIER ON
 GO
 -- =============================================
--- Description: ”ровень изол¤ции : Read Committed
+-- Description: Уровень изол¤ции : Read Committed
 -- =============================================
 CREATE PROCEDURE GetCustomerSalesSumByID
 	@CustomerID int
@@ -59,8 +79,7 @@ BEGIN
 	
 	IF @CustomerID is null
 		BEGIN
-			--RAISERROR(N'ќЎ»Ѕ ј:ѕередан пустой идентификатор покупател¤', 12, 1);
-			PRINT N'ќЎ»Ѕ ј:ѕередан пустой идентификатор покупател¤'
+			PRINT N'Ошибка:ѕередан пустой идентификатор покупателя'
 			RETURN
 		END
 
@@ -80,12 +99,12 @@ GO
 Exec GetCustomerSalesSumByID @CustomerID = 5
 GO
 
-/*3) —оздать одинаковую функцию и хранимую процедуру, посмотреть в чем разница в производительности и почему.*/
+/*3) Создать одинаковую функцию и хранимую процедуру, посмотреть в чем разница в производительности и почему.*/
 IF  OBJECT_ID ( 'dbo.uFN_GetCustomerSalesSumByID','FN' ) IS NOT NULL   
     DROP FUNCTION dbo.uFN_GetCustomerSalesSumByID;  
 GO  
 -- =============================================
--- Description: ”ровень изол¤ции : Read Committed
+-- Description: Уровень изол¤ции : Read Committed
 -- =============================================
 CREATE FUNCTION uFN_GetCustomerSalesSumByID (@CustomerID int)
 RETURNS DECIMAL (18,2)
@@ -111,7 +130,7 @@ SET STATISTICS TIME on
 Select dbo.uFN_GetCustomerSalesSumByID(6) as SaleSum;
 Exec GetCustomerSalesSumByID @CustomerID = 6;
 
-/*4) —оздайте табличную функцию покажите как ее можно вызвать дл¤ каждой строки result set'а без использовани¤ цикла. */
+/*4) Создайте табличную функцию покажите как ее можно вызвать для каждой строки result set'а без использовани¤ цикла. */
 
 IF  OBJECT_ID ( 'dbo.uFN_GetCustomerSalesSumByIDTable','IF' ) IS NOT NULL   
     DROP FUNCTION dbo.uFN_GetCustomerSalesSumByIDTable;  
@@ -145,4 +164,4 @@ cross apply  dbo.uFN_GetCustomerSalesSumByIDTable(Customers.CustomerID)  as sums
 
 --¬о всех процедурах, в описании укажите дл¤ преподавател¤м
 --5) какой уровень изол¤ции нужен и почему. 
---ƒл¤ всех процедур и функций подойдет уровень изол¤ции Read Committed, т.к. требуетс¤ однократно прочитать подтвержденные данные на момент вызова процедур, при этом изменени¤ строк не планируетс¤.
+--Для всех процедур и функций подойдет уровень изол¤ции Read Committed, т.к. требуетс¤ однократно прочитать подтвержденные данные на момент вызова процедур, при этом изменени¤ строк не планируетс¤.
